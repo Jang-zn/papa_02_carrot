7b0525d8e684766c93045a168b662baab1154e54
Jang <zn2309@naver.com>
2022-04-18 01:13:36 +0900

    PageView - 슬라이드하면 화면 옆으로 이동하는 위젯

--- lib/router/router_delegate.dart
+++ lib/router/router_delegate.dart
@@ -7,11 +7,13 @@ final routerDelegate = BeamerDelegate(
       HomeLocation(),
     ],
   ),
+
   //페이지 이동시 유효성검사 진행하는 guard
   guards: [
     BeamGuard(
       pathPatterns: ['/'],
       check: (context, location) {
+        //유효성검사 로직
         return false;
       },
       beamToNamed: (origin, target)=>'/auth',
