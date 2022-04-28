d8017dbf77eb5010a4b3a0cbf9e987eb2f3f40ca
Jang <zn2309@naver.com>
2022-04-28 17:25:47 +0900

    address page button 추가

--- lib/views/start/address_page.dart
+++ lib/views/start/address_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddressPage extends StatelessWidget {
@@ -9,26 +10,44 @@ class AddressPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       minimum: EdgeInsets.all(24),
-      child: Column(children: [
-        TextFormField(
-          decoration: InputDecoration(
-              prefixIcon: Icon(
-                Icons.search,
-                color: Colors.grey,
-              ),
-              prefixIconConstraints:
-                  BoxConstraints(minWidth: 24, minHeight: 24),
-            border: UnderlineInputBorder(
-              borderSide: BorderSide(color:Colors.grey),
-            ),
-            focusedBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color:Colors.lightGreen),
-            ),
-            hintText: '주소 검색',
-            hintStyle: TextStyle(color: Theme.of(context).hintColor)
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+                prefixIconConstraints:
+                    BoxConstraints(minWidth: 24, minHeight: 24),
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.lightGreen),
+                ),
+                hintText: '주소 검색',
+                hintStyle: TextStyle(color: Theme.of(context).hintColor)),
           ),
-        )
-      ],),
+          SizedBox(
+            height: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextButton.icon(
+                  style: TextButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor),
+                  onPressed: () {},
+                  icon: Icon(CupertinoIcons.compass, color: Colors.white),
+                  label: Text(
+                    '현재위치로 찾기',
+                    style: TextStyle(color: Colors.white),
+                  )),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
