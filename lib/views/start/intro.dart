fcf32f580680900e7068a16aebb673bb37a53aaa
Jang <zn2309@naver.com>
2022-04-28 13:28:36 +0900

    LayoutBuilder 사용 *Positioned 사용시 width / left, right 중 중 하나는 null이어야 한다. 동시에 다 사용 못함

--- lib/views/start/intro.dart
+++ lib/views/start/intro.dart
@@ -11,63 +11,73 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            const Text(
-              "Tomato Market",
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.redAccent,
-                  fontWeight: FontWeight.bold),
-            ),
-            Stack(
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        //maybeOf 로 medaiQuery 있는지 없는지도 확인 가능
+        Size size = MediaQuery.of(context).size;
+        final sizeOfposImg = (size.width-32)*0.1;
+        return SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                ExtendedImage.asset('assets/imgs/carrot_intro.png'),
-                Positioned(
-                    child: ExtendedImage.asset(
-                      'assets/imgs/carrot_intro_pos.png',
+                const Text(
+                  "Tomato Market",
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.redAccent,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  width:size.width-32,
+                  child: Stack(
+                    children: [
+                      ExtendedImage.asset('assets/imgs/carrot_intro.png'),
+                      Positioned(
+                        child: ExtendedImage.asset(
+                          'assets/imgs/carrot_intro_pos.png',
+                        ),
+                        left: (size.width-32)*0.45,
+                        right: (size.width-32)*0.45,
+                        top: (size.width-32)*0.45,
+                        bottom: (size.width-32)*0.45,
+                      ),
+                    ],
+                  ),
+                ),
+                const Text(
+                  '우리동네 중고 직거래 토마토마켓',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const Text(
+                  '토마토마켓은 동네 직거래 마켓이에요'
+                      '\n내 동네를 설정하고 시작해보세요',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    TextButton(
+                      child: const Text(
+                        "내 동네 설정하고 시작하기",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: onButtonClick,
+                      style: TextButton.styleFrom(
+                        backgroundColor: Theme.of(context).primaryColor,
+                      ),
                     ),
-                  left: 50,
-                  right: 50,
-                  top: 50,
-                  bottom: 50,
-                ),
+                  ],
+                )
               ],
             ),
-            const Text(
-              '우리동네 중고 직거래 토마토마켓',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const Text(
-              '토마토마켓은 동네 직거래 마켓이에요'
-              '\n내 동네를 설정하고 시작해보세요',
-              style: TextStyle(
-                fontSize: 15,
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                TextButton(
-                  child: const Text(
-                    "내 동네 설정하고 시작하기",
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
-                  ),
-                  onPressed: onButtonClick,
-                  style: TextButton.styleFrom(
-                    backgroundColor: Theme.of(context).primaryColor,
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
