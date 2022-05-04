72cf9a0bd06bc7885a4cf58dabf9a4901656d320
Jang <zn2309@naver.com>
2022-05-04 09:31:55 +0900

    vworld api setting

---
+++ lib/views/start/service/address_service.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:papa_02_carrot/constant/key.dart';
+import 'package:papa_02_carrot/utils/logger.dart';
+
+class AddressService {
+
+  void searchAddressByString(String address) async {
+    //O/1 선택, M/1 필수
+    final formData ={
+      'key': VWORLD_KEY,
+      'request':'search',
+      'query' : address,
+      'type' : 'ADDRESS',
+      'category':'ROAD',
+    };
+
+    var response = await Dio()
+        .get('http://api.vworld.kr/req/search?key=인증키&[검색API 요청파라미터]',
+              queryParameters:formData)
+        .catchError((e) {
+          logger.e(e.message);
+        },
+    );
+    logger.d(response);
+  }
+
+
+}
